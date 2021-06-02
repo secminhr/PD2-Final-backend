f8d163d1d6897a4884bc7d0847a8c3cae7765bb4
Dkoctro <78698451+Dkoctro@users.noreply.github.com>
2021-06-03 03:05:13 +0800

    Update TowerPosition.java

--- src/main/java/ncku/pd2final/Final/tower/TowerPosition.java
+++ src/main/java/ncku/pd2final/Final/tower/TowerPosition.java
@@ -11,17 +11,21 @@ public class TowerPosition {
 	double[][] position = {{lat[0], lat[1], lat[2], lat[3], lat[4], lat[5], lat[6], lat[7], lat[8], lat[9], lat[10], lat[11]},
 			   			   {lng[0], lng[1], lng[2], lng[3], lng[4], lng[5], lng[6], lng[7], lng[8], lng[9], lng[10], lng[11]}};
 	double[][] randomPosition = new double[2][4];
+	double[][] positionAndBlood = new double[3][4];
+	
 	
 	@GetMapping(value = "/towerAndBlood")
-	public void towerAndBlood() {
+	public String towerAndBlood() {
 		CalculateBlood blood = new CalculateBlood();
-		random(position);
+		towerPosition(position);
 		blood.updateBlood();
+		return "Update completed.";
 	}
 	/*
 	 * 儲存所有的堡壘位置，並去隨機堡壘位置
 	 */
-	public void random(double[][] position) {
+	@GetMapping(value = "/towerPosition")
+	public double[][] towerPosition(double[][] position) {
 		//隨機選出四個經緯度
 		int quadrant1 = (int)(Math.random() * 3); //隨機出0~2
 		int quadrant2 = (int)(Math.random() * 3 + 3); //隨機出3~5
@@ -34,9 +38,20 @@ public class TowerPosition {
 			randomPosition[i][2] = position[i][quadrant3];
 			randomPosition[i][3] = position[i][quadrant4];
 		}
+		for(int i = 0; i < 2; i++) {
+			positionAndBlood[i][0] = randomPosition[i][0];
+			positionAndBlood[i][1] = randomPosition[i][1];
+			positionAndBlood[i][2] = randomPosition[i][2];
+			positionAndBlood[i][3] = randomPosition[i][3];
+		}
 		
 		CalculateBlood calculateblood = new CalculateBlood();
 		calculateblood.storePositions(randomPosition);
+		for(int i = 0; i < 4; i++) {
+			positionAndBlood[2][i] = calculateblood.remainBlood[i]; 
+		}
+		
+		return positionAndBlood;
 	}
 	/*
 	 * 隨機堡壘位置，再把出來的東西丟給Blood的class
