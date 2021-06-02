7cabe840540ad11753d8c3d6c149b5e36b362963
1c1y0c8 <f74096255@gs.ncku.edu.tw>
2021-06-03 00:03:33 +0800

    學姊最高

--- src/main/java/ncku/pd2final/Final/attack/Attack.java
+++ src/main/java/ncku/pd2final/Final/attack/Attack.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
 
 @RestController
 public class Attack {
@@ -17,25 +16,38 @@ public class Attack {
     public void attack(@RequestParam double[] lat,
                        @RequestParam double[] lon,
                        @RequestParam double time,
-                       @RequestParam double[] purpose)
+                       @RequestParam double[] purpose,
+                       HttpServletResponse response)
     {
-
         double[][] lAL = new double[lat.length][2] ; //lAL means latitude and longitude
-        for(int i=0; i<lat.length; i++){
-            lAL[i][0] = lat[i] ;
-        }
-        for(int i=0; i<lon.length; i++){
-            lAL[i][1] = lon[i] ;
-        }
+
+//        boolean check = checkData(lat,lon,purpose) ;
+
+        if(dataExist(lat,lon,purpose)){
+
+            for(int i=0; i<lat.length; i++){
+                lAL[i][0] = lat[i] ;
+            }
+            for(int i=0; i<lon.length; i++){
+                lAL[i][1] = lon[i] ;
+            }
+
+            response.setStatus(HttpServletResponse.SC_NO_CONTENT);
+            DistanceCalculate test = new DistanceCalculate() ;
+
+            attackPoints = test.showMessage(lAL)*0.3 ;
+            this.time = time;
+            this.purpose = purpose;
+
+            //呼叫林欣諴的東西
+
+        }else
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
 
 
-        DistanceCalculate test = new DistanceCalculate()   ;
 
-        attackPoints = test.showMessage(lAL)*0.3 ;
-        this.time = time;
-        this.purpose = purpose;
-
-//        System.out.println("System is working!");
+        //System.out.println("System is working!");
+//
 //        for(double latitude : lat){
 //            System.out.println(latitude);
 //        }
@@ -49,29 +61,24 @@ public class Attack {
 
     }
 
-    @GetMapping(value = "/attack")
-    public String checkData(@RequestParam double[] lat,
-                            @RequestParam double[] lon,
-                            @RequestParam double time,
-                            @RequestParam double[] purpose,
-                            HttpServletResponse response) {
-        if (lat != null && lon != null && purpose != null) {
-            response.setStatus(HttpServletResponse.SC_NO_CONTENT);
-            return "{\"success\": true}";
-        } else {
-            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            return "{\"success\": false, \"message\": \"Some arguments leak.\"}";
-        }
 
-}
-    public double showAttackPoints(){
-        return attackPoints;
+    public boolean dataExist(double[] lat,
+                             double[] lon,
+                             double[] purpose) {
+        return lat != null && lon != null && purpose != null;
     }
-    public double showTime(){
-        return time;
-    }
-    public double[] showPurpose(){
-        return purpose;
-    }
+
+
+
+
+//    public double showAttackPoints(){
+//        return attackPoints;
+//    }
+//    public double showTime(){
+//        return time;
+//    }
+//    public double[] showPurpose(){
+//        return purpose;
+//    }
 
 }
