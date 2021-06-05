68585215fe245e651f0f5faf0a910ca63b54770e
Shuo <wangsteven050623@gmail.com>
2021-06-06 00:06:06 +0800

    revise updateblood connect test

--- src/main/java/ncku/pd2final/Final/websocket/UpdateBlood.java
+++ src/main/java/ncku/pd2final/Final/websocket/UpdateBlood.java
@@ -43,23 +43,24 @@ public class UpdateBlood {
     public void onMessage(String message, Session session) {
         JSONObject jsonObject = JSON.parseObject(message);
 
+        int hp = jsonObject.getInteger("hp"); //血量
         double lat = jsonObject.getDouble("lat"); //緯度
         double lng = jsonObject.getDouble("lng"); //經度
-        int hp = jsonObject.getInteger("hp"); //血量
+
 
         Map<String, Object> sendData =  new HashMap<>();
+        sendData.put("hp", hp);
         sendData.put("lat", lat);
         sendData.put("lng", lng);
-        sendData.put("hp", hp);
-
 
         sendMessage(JSON.toJSONString(sendData));
     }
 
-    public void sendMessage(double lat, double lng, int hp)  {
+    public void sendMessage(int hp, double lat, double lng)  {
+        this.hp = hp;
         this.lat = lat;
         this.lng = lng;
-        this.hp = hp;
+
     }
     public void sendMessage(String message){
         this.session.getAsyncRemote().sendText(message);
