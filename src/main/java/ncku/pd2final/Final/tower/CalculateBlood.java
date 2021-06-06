18fea13997481d0666f08e35792d4a5c243046b4
Dkoctro <78698451+Dkoctro@users.noreply.github.com>
2021-06-06 16:19:23 +0800

    Update CalculateBlood.java

--- src/main/java/ncku/pd2final/Final/tower/CalculateBlood.java
+++ src/main/java/ncku/pd2final/Final/tower/CalculateBlood.java
@@ -1,11 +1,16 @@
 package ncku.pd2final.Final.tower;
 
+import ncku.pd2final.Final.websocket.UpdateBlood;
+import ncku.pd2final.Final.websocket.checkGame;
+import ncku.pd2final.Final.websocket.renew;
+
 public class CalculateBlood {
-	int wholeBlood = 2500;
-	double[][] positions = new double[2][4];
-	int[] remainBlood = {wholeBlood, wholeBlood, wholeBlood, wholeBlood};
-	boolean remain = false;
-	double[] gameEnd = {-1, -1, -1};
+	int wholeblood = 2500;                                       			//一開始血量
+	double[][] positions = new double[2][4];					//要儲存隨機的位置
+	int[] remainBlood = {wholeblood, wholeblood, wholeblood, wholeblood};		//一開始四個堡壘的血量
+	boolean remain = false;								//判斷是否還有血量
+	double[] gameEnd = {-1, -1, -1};						//給前端遊戲結束
+	String update = "update complete";						//確認完已經更新完成，就回傳給前端這個訊息
 	
 	public void storePositions(double[][] positions) {
 		for(int i = 0; i < 2; i++) {
@@ -19,82 +24,74 @@ public class CalculateBlood {
 	 */
 	
 	//傳血量、lat、lng
-	public double[] blood(int attackPoints, double[][] attackedPositions, int time) {
+	public void blood(int attackPoints, double[][] attackedPositions, int time) {
+		UpdateBlood updateblood = new UpdateBlood();
+		checkGame check = new checkGame();
+		renew renew = new renew();
+		
 		long neededTime = (long)time;
-		double[] contain = new double[3];
-		try {
-			Thread.sleep(neededTime * 60 * 1000);
-			if(attackedPositions[0][0] == positions[0][0] && attackedPositions[1][0] == positions[1][0]) {
-				remainBlood[0] -= attackPoints;
-				if(remainBlood[0] <= 0)
-					remainBlood[0] = 0;
-				checkBlood(remainBlood);
-				if(remain) {
-					updateTowerAndBlood();
-					return gameEnd;
-				} else {
-					contain[0] = remainBlood[0];
-					contain[1] = attackedPositions[0][0];
-					contain[2] = attackedPositions[1][0];
-				}
-					return contain;
-			} else if(attackedPositions[0][0] == positions[0][1] && attackedPositions[1][0] == positions[1][1]) {
-				remainBlood[1] -= attackPoints;
-				if(remainBlood[1] <= 0)
-					remainBlood[1] = 0;
-				checkBlood(remainBlood);
-				if(remain) {
-					updateTowerAndBlood();
-					return gameEnd;
-				} else{
-					contain[0] = remainBlood[1];
-					contain[1] = attackedPositions[0][0];
-					contain[2] = attackedPositions[1][0];
-				}
-					return contain;
-			} else if(attackedPositions[0][0] == positions[0][2] && attackedPositions[1][0] == positions[1][2]) { 
-				remainBlood[2] -= attackPoints;
-				if(remainBlood[2] <= 0)
-					remainBlood[2] = 0;
-				checkBlood(remainBlood);
-				if(remain) {
-					updateTowerAndBlood();
-					return gameEnd;
-				} else {
-					contain[0] = remainBlood[2];
-					contain[1] = attackedPositions[0][0];
-					contain[2] = attackedPositions[1][0];
-				}
-					return contain;
-			} else if(attackedPositions[0][0] == positions[0][3] && attackedPositions[1][0] == positions[1][3]) {
-				remainBlood[3] -= attackPoints;
-				if(remainBlood[3] <= 0)
-					remainBlood[3] = 0;
-				checkBlood(remainBlood);
-				if(remain) {
-					updateTowerAndBlood();
-					return gameEnd;
-				} else {
-					contain[0] = remainBlood[3];
-					contain[1] = attackedPositions[0][0];
-					contain[2] = attackedPositions[1][0];
-				}
-					return contain;
-			} else
-				return null;
-		} catch (InterruptedException e) {
-			return null;
+		
+		if(attackedPositions[0][0] == positions[0][0] && attackedPositions[1][0] == positions[1][0]) {
+			remainBlood[0] -= attackPoints;
+			if(remainBlood[0] <= 0)
+				remainBlood[0] = 0;
+			checkBlood(remainBlood);
+			if(remain == true) {
+				check.sendMessage(gameEnd);	//傳送四個堡壘都沒寫的訊息[-1、-1、-1]
+				updateTowerAndBlood();		//更新血量跟位置
+				renew.sendMessage(update); 	//回傳新的血量跟位置
+			} else {
+				updateblood.sendMessage(remainBlood[0], attackedPositions[0][0], attackedPositions[1][0]);
+			}
+		} else if(attackedPositions[0][0] == positions[0][1] && attackedPositions[1][0] == positions[1][1]) {
+			remainBlood[1] -= attackPoints;
+			if(remainBlood[1] <= 0)
+				remainBlood[1] = 0;
+			checkBlood(remainBlood);
+			if(remain == true) {
+				check.sendMessage(gameEnd);	//傳送四個堡壘都沒寫的訊息[-1、-1、-1]
+				updateTowerAndBlood();		//更新血量跟位置
+				renew.sendMessage(update); 	//回傳新的血量跟位置
+			} else {
+				updateblood.sendMessage(remainBlood[1], attackedPositions[0][0], attackedPositions[1][0]);
 		}
+			} else if(attackedPositions[0][0] == positions[0][2] && attackedPositions[1][0] == positions[1][2]) { 
+			remainBlood[2] -= attackPoints;
+			if(remainBlood[2] <= 0)
+				remainBlood[2] = 0;
+			checkBlood(remainBlood);
+			if(remain == true) {
+				check.sendMessage(gameEnd);	//傳送四個堡壘都沒寫的訊息[-1、-1、-1]
+				updateTowerAndBlood();		//更新血量跟位置
+				renew.sendMessage(update); 	//回傳新的血量跟位置
+			} else {
+				updateblood.sendMessage(remainBlood[2], attackedPositions[0][0], attackedPositions[1][0]);
+		}
+			} else if(attackedPositions[0][0] == positions[0][3] && attackedPositions[1][0] == positions[1][3]) {
+			remainBlood[3] -= attackPoints;
+			if(remainBlood[3] <= 0)
+				remainBlood[3] = 0;
+			checkBlood(remainBlood);
+			if(remain == true) {
+				check.sendMessage(gameEnd);	//傳送四個堡壘都沒寫的訊息[-1、-1、-1]
+				updateTowerAndBlood();		//更新血量跟位置
+				renew.sendMessage(update); 	//回傳新的血量跟位置
+			} else {
+				updateblood.sendMessage(remainBlood[3], attackedPositions[0][0], attackedPositions[1][0]);
+			}
+		} 
 	}
+
 	/*
 	 * 扣血
- 	 * 會回傳double[血量、lat、lng]，如果遊戲結束，會回傳double[-1、-1、-1]
+ 	 * 會回傳 (int)血量、(double)緯度、(double)經度
+ 	 * 如果遊戲結束，會回傳double[-1、-1、-1]
+ 	 * 然後更新完會傳string="update complete"給前端
 	 */
 	
-	public boolean checkBlood(int[] remainBlood) {
+	public void checkBlood(int[] remainBlood) {
 		if(remainBlood[0] == 0 && remainBlood[1] == 0 && remainBlood[2] == 0 && remainBlood[3] == 0)
 			remain = true;
-		return remain;
 	}
 	/*
 	 * 判斷是否所有血量歸零
@@ -103,17 +100,10 @@ public class CalculateBlood {
 		TowerPosition towerPosition = new TowerPosition();
 		towerPosition.towerAndBlood();
 		for(int i = 0; i < 4; i++) 
-			remainBlood[i] = wholeBlood;
+			remainBlood[i] = wholeblood; 
 		remain = false;
 	}
 	/*
-	 * 更新血量
+	 * 更新血量(可能沒辦法更新到數字，所以需要測試)
 	 */
-	
-	public String update() {
-		return "Update completed.";
-	}
-	/*
-	 * 通知設定完成
-	 */	
 }
